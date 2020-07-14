0989f930e786394e20177ae1fd66e4b65210be57
wizracc <wizracc@gmail.com>
2020-07-14 15:00:44 -0500

    Updates

---
+++ precache-manifest.a2b9494891a38dbae10b596fc002d9b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7717803aa66050efdffbda0203ee8186",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "345ec14d187f671d849d",
+    "url": "/Portfolio/static/css/main.ed1fc8ec.chunk.css"
+  },
+  {
+    "revision": "a007a6bc4b4d4f142543",
+    "url": "/Portfolio/static/js/2.40980b7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.40980b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345ec14d187f671d849d",
+    "url": "/Portfolio/static/js/main.6ea12eb0.chunk.js"
+  },
+  {
+    "revision": "5e8a2624bf0c1e9aa099",
+    "url": "/Portfolio/static/js/runtime-main.d4934111.js"
+  },
+  {
+    "revision": "d7efbf4bc5f2e60442d0284fc0be03e4",
+    "url": "/Portfolio/static/media/clump sample.d7efbf4b.png"
+  },
+  {
+    "revision": "5970f746f9bf17aff6bd3f3d9e63af5a",
+    "url": "/Portfolio/static/media/face pic.5970f746.png"
+  },
+  {
+    "revision": "500ef4adfe55b8f3ebc5ee6bbcbd42a9",
+    "url": "/Portfolio/static/media/grynn sample.500ef4ad.png"
+  },
+  {
+    "revision": "db3831b5e961490da6f1a9354bbbbb14",
+    "url": "/Portfolio/static/media/over the city.db3831b5.jpg"
+  },
+  {
+    "revision": "45e0c3a62487a12ffa1a9d8149f4e6a4",
+    "url": "/Portfolio/static/media/polo sample.45e0c3a6.png"
+  },
+  {
+    "revision": "f55c6df619f501070733c9f0cbf4106e",
+    "url": "/Portfolio/static/media/wait.f55c6df6.jpg"
+  }
+]);
